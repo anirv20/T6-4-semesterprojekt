4a27e1f1f8e4d5f147ad785bba135a4e7706a8c7
AbdullahiMohamed20 <72854449+AbdullahiMohamed20@users.noreply.github.com>
2020-11-02 12:10:27 +0100

    Update Pollution.java

--- worldofzuul/worldofzuul/Pollution.java
+++ worldofzuul/worldofzuul/Pollution.java
@@ -4,7 +4,14 @@ public class Pollution {
     public static final double LIMIT = 500.0;
     private double totalPollution;
     private double turnPollution;
-
+    
+    
+    public Pollution(double LIMIT, double totalpollution, double turnpullotion) {
+        this.limit = LIMIT;
+        this.totalPollution = totalpollution;
+        this.turnpullotion = turnpullotion;
+    }
+    
     public double getLimit() {
         return LIMIT;
     }
